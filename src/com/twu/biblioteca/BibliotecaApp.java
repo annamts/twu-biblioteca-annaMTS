ad67b8bd71f94f1d84df21a2250b6e8b435edc9a
anna <anna.t@thoughtworks.com>
2019-01-24 10:44:28 +0100

    Added welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,10 @@
 package com.twu.biblioteca;
 
+import java.sql.SQLOutput;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }
